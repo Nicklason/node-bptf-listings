d6e7b8639bfa3a42a95837e6ffbd18ad7f5c96d6
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-01-23 18:28:34 +0100

    Removed lines that were not used

--- index.js
+++ index.js
@@ -13,15 +13,8 @@ function Listings(options) {
     this.retry = options.retry || true;
     this.retryTime = options.retryTime || 2 * 1000;
 
-    this.listings = [];
-
     this.request = request;
 }
-
-Listings.prototype.init = function(callback) {
-    var self = this;
-
-};
 
 require('./lib/http.js');
 require('./lib/webapi.js');
