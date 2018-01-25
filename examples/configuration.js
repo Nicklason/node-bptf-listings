2dcc5aa00d21080e756d073eacf382812a5b7828
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-01-25 17:21:46 +0100

    Added configuration example

---
+++ examples/configuration.js
@@ -0,0 +1,6 @@
+var options = {
+    token: '', // Your backpack.tf access token
+    key: '', // Your steam api key
+    retryTime: 2000, // Time to wait before trying to do an action again, in milliseconds
+    waitTime: 1000 // Time to wait before adding a new action, this is used to combine multiple actions into one request
+};
