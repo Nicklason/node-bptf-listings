8852a8cb56d190a207b5f1af5e138e339e845ff4
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2018-01-25 17:15:43 +0100

    Added setup.js example

---
+++ examples/setup.js
@@ -0,0 +1,10 @@
+// Require the module
+var Listings = require('bptf-listings');
+
+var options = {
+    "token": "", // Your backpack.tf access token
+    "key": "" // Your steam api key
+};
+
+// Initialize the constructor with the options, you are now ready to go :)
+var listings = new Listings(options);
